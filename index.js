19eb6a307f03694fb8594e264296ab6caa875370
Tuyen Dang <tuyendq@gmail.com>
2021-09-02 16:04:40 +0700

    First setup /graphql

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql'
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('GraphQL is an amazint Query Language.')
 });
+
+const root = { hello: () => "Hi Money"};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(8000, () => console.log('Running on server port localhost:8000/graphql'));
